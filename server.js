a1af832decfd138c8207e6ea36306ebddec50e17
unknown <XX913@ccs.local>
2025-03-10 17:35:24 +0800

    Initial commit - Express.js server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Express is working! Write your full name'});
+
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
